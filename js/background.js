43a7beb96cf4a1f72ac01a462527b4a41bacd5e7
ee <ee@cnclub.asia>
2018-05-02 04:12:52 +0800

    :tada: initial commit

---
+++ js/background.js
@@ -0,0 +1,19 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(request) {
+        var url = request.url.replace('steamcdn-a.akamaihd.net', 'steamcdn.addones.org');
+        url = url.replace('steamstore-a.akamaihd.net', 'steamcdn.addones.org');
+        url = url.replace('steamcommunity-a.akamaihd.net', 'steamcdn.addones.org');
+        return {redirectUrl: url};
+    },
+    {
+        urls: [
+            "*://steamcdn-a.akamaihd.net/steam/*",
+            "*://steamcdn-a.akamaihd.net/steamcommunity/*",            
+            "*://steamstore-a.akamaihd.net/public/shared/images/*",
+            "*://steamstore-a.akamaihd.net/public/images/*",
+            "*://steamcommunity-a.akamaihd.net/public/shared/images/*",
+            "*://steamcommunity-a.akamaihd.net/public/images/*"
+        ]
+    },
+    ["blocking"]
+);
