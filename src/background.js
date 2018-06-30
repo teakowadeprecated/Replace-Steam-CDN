d59e76802f8eb62946f26733b9859312f23d719f
Ernest E <ee@cnclub.asia>
2018-06-30 18:47:53 +0800

    feat: add broadcast support
    added
    
    - *://steamcdn-a.akamaihd.net/store/promo/*
    - *://steambroadcast.akamaized.net/broadcast/*

--- src/background.js
+++ src/background.js
@@ -1,15 +1,18 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(request) {
-        var url = request.url.replace('steamcdn-a.akamaihd.net', 'steamcdn.addones.org');
-        url = url.replace('steamstore-a.akamaihd.net', 'steamcdn.addones.org');
-        url = url.replace('steamcommunity-a.akamaihd.net', 'steamcdn.addones.org');
-        url = url.replace('cdn.steamstatic.com.8686c.com', 'steamcdn.addones.org');
-        url = url.replace('steamuserimages-a.akamaihd.net', 'steamcdn.addones.org');        
+        var steamcdn = 'steamcdn.addones.org';
+        var url = request.url.replace('steamcdn-a.akamaihd.net', steamcdn);
+        url = url.replace('steamstore-a.akamaihd.net', steamcdn);
+        url = url.replace('steamcommunity-a.akamaihd.net', steamcdn);
+        url = url.replace('cdn.steamstatic.com.8686c.com', steamcdn);
+        url = url.replace('steamuserimages-a.akamaihd.net', steamcdn);        
+        url = url.replace('steambroadcast.akamaized.net', steamcdn);        
         return {redirectUrl: url};
     },
     {
         urls: [
             "*://steamcdn-a.akamaihd.net/steam/*",
+            "*://steamcdn-a.akamaihd.net/store/promo/*",
             "*://steamcdn-a.akamaihd.net/steamcommunity/*",            
             "*://steamstore-a.akamaihd.net/public/shared/images/*",
             "*://steamstore-a.akamaihd.net/public/images/*",
@@ -18,7 +21,8 @@ chrome.webRequest.onBeforeRequest.addListener(
             "*://steamcommunity-a.akamaihd.net/economy/image/*",
             "*://cdn.steamstatic.com.8686c.com/steam/*",
             "*://cdn.steamstatic.com.8686c.com/steamcommunity/*",
-            "*://steamuserimages-a.akamaihd.net/*"
+            "*://steamuserimages-a.akamaihd.net/*",
+            "*://steambroadcast.akamaized.net/broadcast/*"
         ]
     },
     ["blocking"]
